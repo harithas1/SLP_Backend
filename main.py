7e387558f477b9f337a59d117ed4ea4d08266ccd
harithas1 <harithaharitha0862s@gmail.com>
2026-05-01 16:25:05 +0530

    updated cors

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from routes import category, product, order
 import cloudinary
 import cloudinary.uploader
@@ -11,20 +10,29 @@ load_dotenv()
 
 app = FastAPI()
 
+# ✅ FRONTEND URLS
+origins = [
+    "http://localhost:5173",
+    "https://slp-yw6v.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
+# ✅ CLOUDINARY
 cloudinary.config(
     cloud_name=os.getenv("CLOUD_NAME"),
     api_key=os.getenv("CLOUD_API_KEY"),
     api_secret=os.getenv("CLOUD_API_SECRET"),
-    secure=True
+    secure=True,
 )
+
+# ✅ ROUTES
 app.include_router(category.router)
 app.include_router(product.router)
 app.include_router(order.router)
