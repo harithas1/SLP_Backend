6dfd1796e50182d00dbbc6c18eae70b93e74956b
harithas1 <harithaharitha0862s@gmail.com>
2026-05-25 09:42:33 +0530

    Fix admin login whitespace handling today

--- main.py
+++ main.py
@@ -29,16 +29,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-
         "https://srilakshyapublications.netlify.app",
-
         "https://srilakshyapublications.in",
         "https://www.srilakshyapublications.in",
         "http://srilakshyapublications.in",
-        "http://www.srilakshyapublications.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
